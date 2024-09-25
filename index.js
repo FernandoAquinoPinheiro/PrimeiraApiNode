6996394c31187cd4abeeae6932489ba2722bb7ca
FernandoAquinoPinheiro <fernando.pinheiro@etec.sp.gov.br>
2024-09-24 22:06:46 -0300

    Api Finalizada

--- index.js
+++ index.js
@@ -49,8 +49,81 @@ app.post('/person' ,async (req, res)=>{
 
 })
 
+//read by id
+app.get('/person/:id', async (req, res) =>{
+
+    const id = req.params.id
+    try{
+
+        const people = await Person.findOne({_id:id})
+
+        if(!people){
+            res.status(422).json({message: 'Usuario não encontrado'})
+            return
+        }
+        res.status(200).json(people)
+    
+    }catch(error){
+
+        res.status(500).send({erro: error})
+
+    }
+})
+
 //UPDATE
+app.patch("/person/:id",async (req, res)=>{
+
+    const id = req.params.id
+    const {name, salary,approved} = req.body
+
+    const person ={
+        name,
+        salary,
+        approved,
+    }
+
+    try{
+        const updatePerson = await Person.updateOne({_id: id}, person)
+        if(updatePerson.matcheCount ===0){
+
+            res.status(422).json({messsage: "Usuario não encontrado"})
+            return
+        }
+        res.status(200).json(person)
+
+    }catch(error){
+
+        req.status(500).json({erro: error})
+
+    }
+
+})
+
 //DELETE
+app.delete("/person/:id",async (req, res)=>{
+    const id = req.params.id
+    
+    const person = await Person.findOne({_id: id})
+    
+    if (!person){
+
+        res.status(422).json({message: "Usuario não encontrado"})
+        return
+
+    }
+    try{
+
+        await Person.deleteOne({_id: id})
+        res.status(200).json({message: "Usuario removido com sucesso"})
+
+    }
+    catch (error){
+
+        res.status(500).json({erro: error})
+    }
+
+
+})
 
 
 
